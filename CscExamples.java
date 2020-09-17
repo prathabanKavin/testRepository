448cffc2ba1c6858702b451a628ec2af7fae78ff
prathabanKavin <kavinprathaban025@gmail.com>
2020-09-18 05:16:13 +0530

    getter and setter in phy

---
+++ CscExamples.java
@@ -0,0 +1,29 @@
+public class CscExamples{
+	private int questionNo;
+	private String category;
+	private String text;
+	
+	public void setQuestionNo(int questionNo){
+		this.questionNo = questionNo;
+	}
+	
+	public void setCategory(String category){
+		this.category = category;
+	}
+	
+	public void setText(String text){
+		this.text = text;
+	}
+	
+	public int getQuestionNo(){
+		return questionNo;
+	}
+	
+	public String getCategory(){
+		return category;
+	}
+	
+	public String getText(){
+		return text;
+	}
+}
