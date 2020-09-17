83c449d4f922dfca85336d28f3ea8b6ad9875489
prathabanKavin <kavinprathaban025@gmail.com>
2020-09-18 05:23:38 +0530

    add,remove methods from csc

---
+++ CscExamples.java
@@ -0,0 +1,41 @@
+public class CscExamples{
+	private int questionNo;
+	private String category;
+	private String text;
+	
+	public void setQuestionNo(int questionNo){
+		this.questionNo = questionNo;
+	}
+	
+	public void setCategory(String category){
+		this.category = category;
+	}
+	
+	public void setText(String text){
+		this.text = text;
+	}
+	
+	public int getQuestionNo(){
+		return questionNo;
+	}
+	
+	public String getCategory(){
+		return category;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	public void addQuestion(CscExamples question){
+		
+	}
+	
+	public void removeQuestion(int quesId){
+		
+	}
+	
+	public void editQuestion(int quesId){
+		
+	}
+}
